cc9149b7fc26628058be7f5746c4fb65491d4466
bhatia12 <shubh.bhatia800@gmail.com>
2020-01-06 23:59:44 +0530

    carpage

---
+++ src/components/table.svelte
@@ -0,0 +1,17 @@
+<script>
+
+
+</script>
+
+
+<style>
+
+
+</style>
+
+
+    <div class = "flex">
+    
+    
+    </div>
+
---
+++ src/routes/index.svelte
@@ -0,0 +1,123 @@
+<script>
+
+</script>
+
+<style>
+  .top{
+    background-color: black;
+    color:#D4AF37;
+    width:full;
+    
+    width:100%;
+    text-align: right;
+    
+    
+    
+  }
+  .margin{
+    margin-left:10%;
+    padding:5%
+    
+  }
+  .bottom{
+    width:100%;
+    margin-bottom:2%
+    
+    
+  }
+  .data{
+    margin-top: 2%;
+    text-align:center
+    
+  }
+  .shadow1 {
+    box-shadow: 0px 15px 50px #bdbdbd50;
+  }
+  
+
+
+</style>
+
+
+  <div class="w-full text-center top">
+    <div class= "mr-4">
+      <p>(516)365-5400</p>
+    </div>
+  <div class="flex margin">
+    <div class=""> 
+      <p>About Us</p>
+    </div>
+    <div class="ml-12">
+      <p>Location</p>
+    </div>
+    <div class="ml-12">
+      <p>Gallery</p>
+    </div>
+    <div class="mb-2">
+      <img alt="logo" src="img/logo.jpg">
+    </div>
+    <div class="ml-12">
+      <p>Blog</p>
+    </div>
+    <div class="ml-12">
+      <p>Services</p>
+    </div>
+    <div class="ml-12">
+      <p>Contact Us</p>
+    </div>
+
+  </div >
+
+  <div>
+    <img class="mb-12" alt="car" src="img/car.jpg">
+  </div>
+  </div>
+  <div class="bottom ">
+    <div class="data">
+      <div class="font-bold">
+        <p>REPAIR SERVICES</p>
+        
+      </div>
+      <div class="text-gray-600 mt-1">
+       <p>From Door Dings to Fender Benders and Major Accidents</p>
+      </div>
+      <div class=" flex mt-4 item-center ">
+        <div class="w-1/3 px-4 py-8   shadow1 text-white bg-orange-500 ml-4 rounded-lg shadow-2xl">
+          <p class="font-semibold">COLLISION</p>
+          <p>REPAIR</p> 
+        
+        </div>
+        <div class="w-1/3 px-4 text-white py-8 ml-4 shadow-xl shadow1 bg-green-300 rounded-lg">
+          <p class="font-semibold">FENDER</p>
+          <p>REPAIR</p>
+        </div>
+        <div class="w-1/3  px-4 text-white py-8 ml-4 shadow1 shadow-xl rounded-lg bg-teal-600">
+          <p class="font-semibold">DENT</p>
+          <p>REPAIR</p>
+        </div>
+      </div>
+      <div class="flex mt-4 ">
+        <div class="w-1/3 px-4 text-white py-8 shadow1 shadow1 bg-blue-500 ml-4 rounded-lg shadow-2xl">
+          <p class="font-semibold">PAINT</p>
+          
+          <p>REPAIR</p>
+        </div>
+        <div class="w-1/3 px-4 text-white py-8 ml-4 shadow1 shadow-xl bg-indigo-400 rounded-lg">
+          <p class="font-semibold">WINDSHEILD</p>
+          <p>REPAIR</p>
+           
+        </div>
+        <div class="w-1/3 px-4 text-white py-8 ml-4 shadow1 shadow-xl bg-purple-500 rounded-lg">
+          <p class="font-semibold">BUMPER</p>
+          <p>REPAIR</p>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  
+
+  
